6223c0efcfd5266b061ad3271142c570285c9815
longlong.bai <longlong.bai@aliyun.com>
2020-02-28 18:23:35 +0800

    富文本编辑器

--- jcart-administration-front/viewmodels/product-search.js
+++ jcart-administration-front/viewmodels/product-search.js
@@ -1,25 +1,32 @@
 var app = new Vue({
     el: '#app',
     data: {
-        pageInfo:'',
-        pageNum:1
+        pageInfo: '',
+        pageNum: 1
     },
     mounted() {
-        console.log("view mounted")
-        this.getsearch();
+        console.log('view mounted');
+        this.searchProduct();
     },
     methods: {
-        getsearch(){
-            axios.get("/product/search",{
-                param:{
-                    pageNum:this.pageNum
+        handlePageChange(val){
+            console.log('page change');
+            this.pageNum = val;
+            this.searchProduct();
+        },
+        searchProduct() {
+            axios.get('/product/search', {
+                params: {
+                    pageNum: this.pageNum
                 }
-            }).then(function (response){
-                console.log(response)
-                app.pageInfo = response.data.list;
-            }).catch(function (error){
-                console.log(error)
             })
+                .then(function (response) {
+                    console.log(response);
+                    app.pageInfo = response.data;
+                })
+                .catch(function (error) {
+                    console.log(error);
+                });
         }
-    },
+    }
 })
